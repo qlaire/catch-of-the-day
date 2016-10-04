e88cf5e7a564af4ca93b14f51c0a6cded29cf20a
Claire Dranginis <cdranginis@gmail.com>
2016-10-04 14:53:35 -0400

    app completed

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDUTYBi6aUZ4jd0dXHrbXZqpSFHZuy575k",
+  authDomain: "catch-of-the-day-by-qlaire.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-by-qlaire.firebaseio.com",
+});
+
+export default base;
